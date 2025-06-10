e8c3e990c83adff8919deb3bfc3fcd8ef0ccb295
Hrishikesh Sarma <hrishikeshsarma4444@gmail.com>
2025-06-10 21:18:22 +0530

    Added backend url

--- frontend/src/pages/solarForecast/solarForecast.jsx
+++ frontend/src/pages/solarForecast/solarForecast.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./SolarForecast.css";
+import "./solarForecast.css";
 
 const SolarForecast = () => {
   const [time, setTime] = useState("");
